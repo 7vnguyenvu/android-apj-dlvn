173c23ad0e455718b88d3d48c5cd6f6bcc33d80b
Vune <Vune@gmail.com>
2023-01-11 12:52:12 +0700

    Điều chỉnh Fragment_Find lần thứ n (100%)

--- DU_LICH_VN/app/src/main/java/com/example/du_lich_vn/Fragment_Find.java
+++ DU_LICH_VN/app/src/main/java/com/example/du_lich_vn/Fragment_Find.java
@@ -30,8 +30,13 @@ public class Fragment_Find extends Fragment  {
     View view;
     private Activity_Main _ActivityMain;
     Context _ThisContext;
-    ArrayList<Class_Place> arrLocation=new ArrayList<Class_Place>();
-    ArrayList<Class_Place> find= new ArrayList<>();
+    ArrayList<Class_Place> arrLocation =new ArrayList<Class_Place>();
+    private ArrayList<Class_Place> finds = new ArrayList<>();
+
+    public void setFinds(ArrayList<Class_Place> finds) {
+        this.finds = finds;
+    }
+
     private SearchView searchView;
 
     @Override
@@ -42,36 +47,51 @@ public class Fragment_Find extends Fragment  {
     }
 
     private void mysearch(String s) {
-        find.clear();
-        for (Class_Place fl:arrLocation)
-            if(fl.getName().toLowerCase().contains(s.toLowerCase()))
-                find.add(fl);
+        if (!s.equals("")){
+            System.out.println("Đang tìm: " + s);
 
-        Adapter_PlaceFind locationAdapter_Search = new Adapter_PlaceFind(getActivity().getApplicationContext(),R.layout.place_item_of_find_custom,find);
+            ArrayList<Class_Place> tmps = new ArrayList<>();
+            for (Class_Place place:finds) {
+                if(place.getName().toLowerCase().contains(s.toLowerCase())) {
+                    tmps.add(place);
+                }
+            }
 
-        if(!find.isEmpty())
-            lv.setAdapter(locationAdapter_Search);
+            if(!tmps.isEmpty()) {
+                setFinds(tmps);
+            }
+        } else {
+            setFinds(arrLocation);
+        }
+        Adapter_PlaceFind locationAdapter_Search = new Adapter_PlaceFind(_ThisContext,R.layout.place_item_of_find_custom, finds);
+        lv.setAdapter(locationAdapter_Search);
+
+
     }
+
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        arrLocation = _ActivityMain.getPlaces();
+        setFinds(arrLocation);
+
         searchView=view.findViewById(R.id.Search_location);
         searchView.clearFocus();
 
+        Adapter_PlaceFind placeAdapter = new Adapter_PlaceFind(getActivity().getApplicationContext(),R.layout.place_item_of_find_custom, finds);
+
         lv=view.findViewById(R.id.lvfind);
-        arrLocation= _ActivityMain.getPlaces();
-
-        Adapter_PlaceFind locationAdapter = new Adapter_PlaceFind(getActivity().getApplicationContext(),R.layout.place_item_of_find_custom,arrLocation);
-
-        lv.setAdapter(locationAdapter);
+        lv.setAdapter(placeAdapter);
 
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 Intent detail_place = new Intent(_ThisContext, Activity_DetailPlace.class);
                 Bundle bundle = new Bundle();
-                bundle.putSerializable("classPlace", find.get(i));
+                for (Class_Place place :arrLocation)
+                    if(place.getCode().equals(finds.get(i).getCode()))
+                        bundle.putSerializable("classPlace", place);
                 detail_place.putExtra("bundle", bundle);
                 startActivity(detail_place);
             }
@@ -85,6 +105,7 @@ public class Fragment_Find extends Fragment  {
 
             @Override
             public boolean onQueryTextChange(String s) {
+                System.out.println(s);
                 mysearch(s);
                 return true;
             }
